3e360e2671faa382ab03a06f0a362b9e66211201
AnjaliBharwani <acksacks2405@gmail.com>
2023-12-21 13:16:33 +0530

    submitting code-Thu Dec 21 2023 13:16:32 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,107 +1,79 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
+let relations = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"]
 const App = () => {
-    let [firstName, setFirstName] = useState("");
-    let [secondName, setSecondName] = useState("");
-    let [relationshipStatus, setRelationshipStatus] = useState("");
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+const [printResult, setPrintResult] = useState(false)
+  const [relationShip, setRelationShip] = useState("");
+  console.log(name1, name2);
 
-    function relationshiohandler(){
-        //check if any input is empty
-        if(!firstName || !secondName){
-            setRelationshipStatus("Please Enter valid input")
-        }
+  function calculateRelationShip(e) {
+    e.preventDefault();
+    // for(let ch of name1){
+    //     if (name2.includes(ch)) {
+    //         setName1(name1.replace(ch, ""))
+    //         setName2(name2.replace(ch, ""))
+    //     }
+    // }
+    let str1 = name1;
+    let str2 = name2;
 
-        //compare input string Input should be considered case-sensitive.
-
-        let commonChars = []; //empty array
-        let name1Char = firstName.split("");//split input in aray
-        let name2Char = secondName.split("")//split input in aray
-
-        //for of loop to compare
-        for(let char of name1Char){
-
-            // Here, name2Chars is an array containing characters from the second name (name2), and char represents a character from the first name (name1). The indexOf method is used to check if the character char exists in the name2Chars array. If it does, index will be the position (index) of that character in name2Chars, and if it doesn't exist, index will be -1. This line effectively compares characters between name1 and name2 to find common characters.
-            const index = name2Char.indexOf(char);
-
-            if(index !== -1){
-                commonChars.push(char);
-//The line name2Chars.splice(index, 1); is used to remove a character from the name2Chars array at a specific index, which corresponds to the common character found in both name1 and name2.
-
-// Here's a breakdown of how this line works:
-
-//     name2Chars is an array that contains the characters from the second name, name2.
-
-//     index represents the index (position) of a common character (found in both name1 and name2) within the name2Chars array.
-
-//     The splice method is called on the name2Chars array. It is used to modify the contents of an array by adding or removing elements. In this case, we're using it to remove a single element from the array.
-
-//     The first argument to splice is the index at which you want to start making changes.
-
-//     The second argument is the number of elements to remove. In this case, we are removing only one element, which is the common character found at the specified index.
-
-// So, when name2Chars.splice(index, 1); is executed, it removes the common character found at the index position from the name2Chars array. This ensures that each common character is counted only once, as specified in your original logic for the "FLAMES" relationship calculation.
-                name2Char.splice(index, 1);
-            }
-        }
-
-        const remainingCharCount = name1Char.length + name2Char.length;
-        const relationshipValue = remainingCharCount % 6
-
-        switch(relationshipValue){
-            case 1:
-                setRelationshipStatus("Friends");
-                break;
-            case 2:
-                setRelationshipStatus("Love")
-                break;
-            case 3:
-                setRelationshipStatus("Affection")
-                break;
-            case 4:
-                setRelationshipStatus("Marriage")
-                break;
-            case 5:
-                setRelationshipStatus("Enemy")
-                break;
-            case 0:
-                setRelationshipStatus("Siblings")
-                break;
-            default:
-                setRelationshipStatus("")
-        }
-
-    };
-
-    function delethandle(){
-        setFirstName("")
-        setSecondName("")
-        setRelationshipStatus("")
-    }
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-               <h1>Flame app</h1>
-               <input data-testid="input1"
-                placeholder="First Name"
-                value={firstName}
-                onChange={(e)=> setFirstName(e.target.value)}
-                name="name1"/>
-
-               <input data-testid="input2"
-               placeholder="Second Name"
-               value={secondName}
-               onChange={(e)=>setSecondName(e.target.value)}
-               name="name2"/>
-               
-               <button data-testid="calculate_relationship" onClick={relationshiohandler}>
-                Calculate Relationship Future</button>
-               <button data-testid="clear" onClick={delethandle}>Clear</button>
-               <h3 data-testid="answer">{relationshipStatus}</h3>
-            </div>
-        )
+    for (let ch of str1) {
+      if (str2.includes(ch)) {
+        str1 = str1.replace(ch, "");
+        str2 = str2.replace(ch, "");
+      }
     }
 
+    console.log(str1, str2)
+    setName1(str1);
+    setName2(str2);
+    setPrintResult(true)
+    // setRelationShip(relations[(str1.length + str2.length)%6]);
 
+  }
+
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        name = "name1test"
+        placeholder="First Name"
+        data-testid="input1"
+        onChange={(e) => setName1(e.target.value)}
+        value={name1}
+      />
+      <input
+        type="text"
+        name = "name2"
+        placeholder="Second Name"
+        data-testid="input2"
+        onChange={(e) => setName2(e.target.value)}
+        value={name2}
+      />
+      <button
+        onClick={calculateRelationShip}
+        data-testid="calculate_relationship"
+      >
+        Calculate Relationship Future
+      </button>
+      <button data-testid="clear" onClick={()=>{
+        setPrintResult(false);
+        setName1("");
+        setName2("");
+
+
+      }}>Clear</button>
+      <h3 data-testid="answer">
+        {
+            printResult && relations[(name1.length+name2.length)%6]
+        }        
+      </h3>
+    </div>
+  );
+};
 
 export default App;
